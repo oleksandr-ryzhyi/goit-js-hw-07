7a89a8f4c3ffc44613851388c1f6e431b2bace54
Oleksandr Ryzhyi <alexandrryzhiy@gmail.com>
2020-12-12 16:16:27 +0200

    ДЗ-7: сделал задачу 1

--- js/task-1.js
+++ js/task-1.js
@@ -38,3 +38,12 @@
 // Например для первой категории получится:
 // Категория: Животные
 // Количество элементов: 4
+
+const li = document.querySelectorAll('li.item');
+    
+console.log(`В списке ${li.length} категории.`);
+
+li.forEach(function (categoryItem) {
+  console.log(`Категория: ${categoryItem.querySelector('h2').textContent}`);
+  console.log(`Количество элементов: ${categoryItem.querySelectorAll('li').length}`)
+})
